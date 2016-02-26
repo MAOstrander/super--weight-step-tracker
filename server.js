dd21329fffbd204110faa233eb7107c68c6c1904
seforarodriguez <seforarodriguez13@gmail.com>
2016-02-26 11:22:25 -0600

    Database Created

--- server.js
+++ server.js
@@ -3,12 +3,17 @@
 const express = require('express');
 const sqlite3 = require('sqlite3');
 
+const db = new sqlite3.Database('./db/TrackerThatTracks.db')
+
 const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.get('/', (req, res) => {
 
+app.get('/', (req, res) => {
+  db.all(`SELECT * FROM user`, (err, data) => {
+    console.log(data)
+  })
 });
 
 app.listen(PORT, () => {
