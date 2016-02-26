ad7a68095247481913662f6d83560035b6ecb109
Matt Hays <mdhays1995@gmail.com>
2016-02-26 15:19:46 -0600

    Commiting to merge.

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
 const express = require('express');
 const sqlite3 = require('sqlite3');
 
-const db = new sqlite3.Database('./db/TrackerThatTracks.db')
+const db = new sqlite3.Database('./db/TrackerThatTracks.db');
 
 const PORT = process.env.PORT || 3000;
 
@@ -13,18 +13,23 @@ app.set('view engine', 'jade');
 
 app.get('/', (req, res) => {
   res.render('index');
-  // db.run(`INSERT INTO weight VALUES (1, 1, 'March 17', 150)`, (err, data) => {
-  //   if (err) throw err;
+  db.run(`INSERT INTO weight VALUES (1, 1, 'March 17', 150)`, (err, data) => {
+    if (err) throw err;
 
-  //   res.send(data);
-  // });
+    res.send(data);
+  });
 
-  // db.all(`SELECT * FROM user`, (err, data) => {
-  //   if (err) throw err;
+  db.all(`SELECT * FROM user`, (err, data) => {
+    if (err) throw err;
 
-  //   res.send(data);
-  // })
+    res.send(data);
+  })
 });
+
+app.post('/', (req, res) => {
+  const username = req.body.username;
+});
+
 
 app.listen(PORT, () => {
   console.log(`Server listening on port: ${PORT}`);
